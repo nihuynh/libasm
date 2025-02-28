06113f980eb7ba60130171bf15f1d22969ab94f5
Nicolas Huynh <nico.huynh@gmail.com>
2025-02-28 23:01:47 +0100

    pedentic

--- clone/include/libasm.h
+++ clone/include/libasm.h
@@ -6,7 +6,7 @@
 /*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 15:22:05 by nihuynh           #+#    #+#             */
-/*   Updated: 2025/02/23 23:08:10 by nihuynh          ###   ########.fr       */
+/*   Updated: 2025/02/28 22:25:11 by nihuynh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ typedef struct s_list
 int     ft_atoi_base(char *str, char *base);
 void    ft_list_push_front(t_list **begin, void *elt);
 int     ft_list_size(t_list **begin);
-void    ft_list_sort(t_list **begin, int (*cmp)());
-void    ft_list_remove_if(t_list **begin, void *ref, int (*cmp)(), void (*free_fn)(void*));
+void    ft_list_sort(t_list **begin, int (*cmp)(void));
+void    ft_list_remove_if(t_list **begin, void *ref, int (*cmp)(void), void (*free_fn)(void*));
 
 #endif
